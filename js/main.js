be116d10f47d4bccbfd37c1659a040798b3bd817
Phipsiart <98510944+Phipsiart@users.noreply.github.com>
2023-05-21 21:45:02 +0200

    Revert "hopefully this works"
    This reverts commit 76d9ede56e900cc63572e4b9f8f2b9d93ace9aec.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 let enoughornot = () => {
     let LeberkasCount = document.getElementById("LeberkasCount").value;
     let answerSection = document.getElementById("answerSection");
-    let Map = document.querySelector(".butcher-map")
+    let Map = document.getElementById("meineKarte");
+
     if (LeberkasCount < 3) {
         answerSection.innerHTML = "Um Gottes Willen! Du brauchst jetzt Leberkäse. Hier findest du Metzgereien:"
-        Map.classList.remove("map-hidden");
-        Map.classList.toggle("map-visible"); 
+        Map.style.visibility = "visible";
     } else if (LeberkasCount < 7) {
         answerSection.innerHTML = "<br><br>Basst. Du host gnua Lebakas."
-        Map.classList.remove("map-visible");
-        Map.classList.toggle("map-hidden"); 
+        Map.style.visibility = "hidden";
     } else if (LeberkasCount >= 7) {
         answerSection.innerHTML = "<br><br>Sappralot! Da werd da ja schlecht!"
-        Map.classList.remove("map-visible");
-        Map.classList.toggle("map-hidden"); 
+        Map.style.visibility = "hidden";
     }
 }
